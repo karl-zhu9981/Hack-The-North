18071691fc12e2a0a11957fb0f4fd1836fe604b8
Your Name <felicia.liang@mail.utoronto.ca>
2019-09-14 14:58:20 -0400

    distance calculation for geopoints, type conversions implemented and tested, json conversion of results and printing status added

--- BigQueryFunctions.py
+++ BigQueryFunctions.py
@@ -3,14 +3,15 @@ from google.cloud import bigquery
 
 
 class Bigquery_functions:
-    def init(self, project_id, dataset_id, table_id, current_location):
+    def __init__(self, project_id, dataset_id, table_id, current_location):
         """
         initilizes the service class
         """
-        self.client = bigquery.Client()
+        self.client = bigquery.Client(project=project_id)
         self.project_id = project_id
         self.dataset_id = dataset_id
         self.table_id = table_id
+        self.current_location = current_location
 
     def return_nearby_locations(self):
         """
@@ -18,7 +19,6 @@ class Bigquery_functions:
         current_location: {latitude: int, longitude: int}
         table_id: model_name
         """
-        project_id = self.project_id
         dataset_id = self.dataset_id
         table_id = self.table_id
         current_location = self.current_location
@@ -26,9 +26,13 @@ class Bigquery_functions:
         # collects only necessary fields from provided data. 
         query_1 = '''
         SELECT
+            name,
             ST_GEOGPOINT(longitude, latitude) as geopoint,
-            recycling institution_name,
-            rebate
+            rebate,
+            estimated_rebate,
+            email,
+            phone,
+            website
         FROM {0}.{1}
         '''.format(dataset_id, table_id)
         table_id_cleaned = table_id + '_cleaned'
@@ -40,18 +44,19 @@ class Bigquery_functions:
            *
         FROM (
             SELECT
-               ST_distance(geopoint, ST_GEOGPOINT({2}, {3})) as distance
+               ROUND((ST_distance(geopoint, ST_GEOGPOINT({2}, {3}))/1000), 1) as distance_km,
+               *
             FROM
                {0}.{1}
             )
-        ORDER BY distance ASC
+        ORDER BY distance_km ASC
         '''.format(dataset_id, table_id_cleaned,
                    current_location['latitude'], current_location['longitude'])
         final_table_id = table_id + '_processed'
         self.create_table_from_query(query_2, dataset_id, final_table_id)
         row_iter = self.convert_table_to_json(dataset_id, final_table_id)
         for row in row_iter:
-            print(row.items)
+            print(row.values())
             # each row is json formatted data
 
     def convert_table_to_json(self, dataset_id, table_id):
@@ -62,18 +67,23 @@ class Bigquery_functions:
         SELECT
             TO_JSON_STRING(t) AS json
         FROM
-            {0}.{1} AS t; 
+            {0}.{1} AS t
         '''.format(dataset_id, table_id)
         self.create_table_from_query(query, dataset_id, table_id + '_json')
         # Collect the data as a json
-        table_id = '{0}.{1}.{2}'.format(project_id, dataset_id, table_id)
+        table_id = '{0}.{1}.{2}'.format(project_id, dataset_id, table_id + '_json')
+        print(table_id)
         table = self.client.get_table(table_id)
-        return table
+        rows = self.client.list_rows(table)
+        return rows
 
     def create_table_from_query(self, query, dataset_id, new_table_id):
+        print('query: {}'.format(query))
         job_config = bigquery.QueryJobConfig()
         table_ref = self.client.dataset(dataset_id).table(new_table_id)
         job_config.destination = table_ref
+        job_config.allow_large_results = True
+        job_config.write_disposition = "WRITE_TRUNCATE"
         query_job = self.client.query(query, location='US', job_config=job_config)
         query_job.result()
         print('table created: {0}'.format(new_table_id))
